842040e9007e134e0cfb2e9d36a0ad8b43bc4fd4
Stoyan Zhelyazkov <zhelyazkovs@vmware.com>
2021-06-20 15:45:53 +0300

    bubble sort impl and test set improvements

---
+++ src/algorithms/bubble/bubble.go
@@ -0,0 +1,22 @@
+package bubble
+
+import (
+	"sort/src/comparator"
+)
+
+func Sort(inputSlice []int, c comparator.Comparator) []int {
+	for i := 0; i < len(inputSlice) - 1; i++ {
+		sorted := true
+		for j := 0; j < len(inputSlice) - 1 - i; j++ {
+			if c(inputSlice[j], inputSlice[j + 1]) < 0 {
+				inputSlice[j + 1], inputSlice[j] = inputSlice[j], inputSlice[j + 1]
+				sorted = false
+			}
+		}
+		if sorted {
+			break
+		}
+	}
+
+	return inputSlice
+}
--- src/testutil/testutil.go
+++ src/testutil/testutil.go
@@ -1,8 +1,12 @@
 package testutil
 
 import (
+	"sort/src/comparator"
+	"sort/src/datasets"
 	"testing"
 )
+
+type Sort func([]int, comparator.Comparator) []int
 
 func areSlicesEqual(a, b []int) bool {
 	if len(a) != len(b) {
@@ -16,8 +20,45 @@ func areSlicesEqual(a, b []int) bool {
 	return true
 }
 
-func TestEquality(actual, expected []int, t *testing.T) {
+func testEquality(actual, expected []int, t *testing.T) {
 	if !areSlicesEqual(actual, expected) {
 		t.Errorf("Slices not equal! Expected %v but got %v", expected, actual)
 	}
+}
+
+func TestSortEmpty(s Sort, t *testing.T) {
+	testEquality(s(datasets.Empty(), comparator.Asc), datasets.Empty(), t)
+	testEquality(s(datasets.Empty(), comparator.Desc), datasets.Empty(), t)
+}
+
+func TestSortOneElement(s Sort, t *testing.T) {
+	testEquality(s(datasets.OneElement(), comparator.Asc), datasets.OneElement(), t)
+	testEquality(s(datasets.OneElement(), comparator.Desc), datasets.OneElement(), t)
+}
+
+func TestSortTwoEqualElements(s Sort, t *testing.T) {
+	testEquality(s(datasets.TwoEqualElements(), comparator.Asc), datasets.TwoEqualElements(), t)
+	testEquality(s(datasets.TwoEqualElements(), comparator.Desc), datasets.TwoEqualElements(), t)
+}
+
+func TestSortTwoElements(s Sort, t *testing.T) {
+	testEquality(s(datasets.TwoElementsAsc(), comparator.Asc), datasets.TwoElementsAsc(), t)
+	testEquality(s(datasets.TwoElementsDesc(), comparator.Asc), datasets.TwoElementsAsc(), t)
+	testEquality(s(datasets.TwoElementsDesc(), comparator.Desc), datasets.TwoElementsDesc(), t)
+	testEquality(s(datasets.TwoElementsAsc(), comparator.Desc), datasets.TwoElementsDesc(), t)
+}
+
+func TestSortEqualElements(s Sort, t *testing.T) {
+	testEquality(s(datasets.EqualElements(), comparator.Asc), datasets.EqualElements(), t)
+	testEquality(s(datasets.EqualElements(), comparator.Asc), datasets.EqualElements(), t)
+}
+
+func TestSortUniqueElements(s Sort, t *testing.T) {
+	testEquality(s(datasets.UnsortedUnique(), comparator.Asc), datasets.SortedUniqueAsc(), t)
+	testEquality(s(datasets.UnsortedUnique(), comparator.Desc), datasets.SortedUniqueDesc(), t)
+}
+
+func TestSortNotUniqueElements(s Sort, t *testing.T) {
+	testEquality(s(datasets.Unsorted(), comparator.Asc), datasets.SortedAsc(), t)
+	testEquality(s(datasets.Unsorted(), comparator.Desc), datasets.SortedDesc(), t)
 }
