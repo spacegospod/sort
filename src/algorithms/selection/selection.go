177631d224694b4e228d852a2395622a2e93a336
Stoyan Zhelyazkov <zhelyazkovs@vmware.com>
2021-06-20 15:17:21 +0300

    implement selection sort

--- src/algorithms/selection/selection.go
+++ src/algorithms/selection/selection.go
@@ -4,6 +4,14 @@ import (
 	"sort/src/comparator"
 )
 
-func Sort(inputSlice []int, comparator comparator.Comparator) []int {
+func Sort(inputSlice []int, c comparator.Comparator) []int {
+	for i := 0; i < len(inputSlice); i++ {
+		for j := i; j < len(inputSlice); j++ {
+			// If comparator condition is not met swap elements
+			if c(inputSlice[i], inputSlice[j]) < 0 {
+				inputSlice[j], inputSlice[i] = inputSlice[i], inputSlice[j]
+			}
+		}
+	}
 	return inputSlice
 }
