07ccfc4d5dad150f71ea9d307fae7046e5a6a053
Stoyan Zhelyazkov <zhelyazkovs@vmware.com>
2021-06-23 18:16:20 +0300

    quick sort

---
+++ src/algorithms/quick/quick.go
@@ -0,0 +1,38 @@
+package quick
+
+import (
+	"sort/src/comparator"
+)
+
+func Sort(inputSlice []int, c comparator.Comparator) []int {
+	if len(inputSlice) > 1 {
+		i := partition(inputSlice, c)
+		if i > 0 {
+			Sort(inputSlice[:i], c)
+		}
+		if i < len(inputSlice) {
+			Sort(inputSlice[i:], c)
+		}
+	}
+
+	return inputSlice
+}
+
+func partition(inputSlice []int, c comparator.Comparator) int {
+	pivot := inputSlice[0]
+	i, j := 1, len(inputSlice) - 1
+
+	for i < j {
+		if c(pivot, inputSlice[i]) < 0 {
+			i++
+		} else {
+			inputSlice[i], inputSlice[j] = inputSlice[j], inputSlice[i]
+			j--
+		}
+	}
+
+	if c(pivot, inputSlice[i]) < 0 {
+		inputSlice[0], inputSlice[i] = inputSlice[i], inputSlice[0]
+	}
+	return i
+}
